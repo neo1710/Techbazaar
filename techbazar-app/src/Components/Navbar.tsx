a129ce7c2c0c59fff570b2c1e1c65ee81710adb0
Samay Dhawale <samaydhawale1@gmail.com>
2023-06-15 01:53:15 +0530

    done with todays task

--- techbazar-app/src/Components/Navbar.tsx
+++ techbazar-app/src/Components/Navbar.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css';
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faPhone,faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import { faPhone,faChevronDown,faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import { } from '@fortawesome/free-brands-svg-icons'
-import { Link } from 'react-router-dom';
+import { Link,NavLink } from 'react-router-dom';
 import Logo from "../Images/Logo.png"
 
 function Navbar() {
+
+    const [isAtTop, setIsAtTop] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsAtTop(scrollPosition >= 20)
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+},[]);
+
+console.log(isAtTop)
+
+
   return (
     <div>
         <div className='greenBarTop'>
@@ -21,15 +41,17 @@ function Navbar() {
                 <div>Location <FontAwesomeIcon icon={faChevronDown} size="sm" /></div>
             </div>
         </div>
-        <div className='Nav'>
+        <div className={isAtTop?"Nav2":"Nav1"}>
             <Link to='/'>
             <img src={Logo} alt="logo" />
             </Link>
-
-            <div>
-                
+            <div className='NavLinkDiv'>
+              <NavLink className="NavLink" to="/products">Products</NavLink>
+              <NavLink className="NavLink" to="">Deals</NavLink>
+              <NavLink className="NavLink" to="">What's New</NavLink>
+              <NavLink className="NavLink" to="">Delivery</NavLink>
             </div>
-           
+            
         </div>
     </div>
   )
