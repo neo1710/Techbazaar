d8480ccb233a744845c307f8d969d2336d059e70
Samay Dhawale <samaydhawale1@gmail.com>
2023-06-14 22:14:41 +0530

    done with todays task

--- techbazar-app/src/App.tsx
+++ techbazar-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HomePage } from './Pages/Homepage';
+import MainRoutes from './Pages/MainRoutes';
 
 function App() {
   return (
     <div className="App">
-   
+      <MainRoutes/>
     </div>
   );
 }
