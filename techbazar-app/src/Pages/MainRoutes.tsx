1c923f346e5d67c1709347a47f480155f027b8d1
Samay Dhawale <samaydhawale1@gmail.com>
2023-06-14 22:21:31 +0530

    done with todays task

--- techbazar-app/src/Pages/MainRoutes.tsx
+++ techbazar-app/src/Pages/MainRoutes.tsx
@@ -1,15 +1,1 @@
-import {Routes, Route} from "react-router-dom"
-import react from "react"
 
-import React from 'react'
-import { HomePage } from "./Homepage"
-
-export default function MainRoutes() {
-  return (
-    <div>
-        <Routes>
-            <Route path="/" element={<HomePage/>}/>
-        </Routes>
-    </div>
-  )
-}
