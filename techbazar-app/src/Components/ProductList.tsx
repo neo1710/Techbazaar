15ba838f8e18616ce3f5778b26cd65f9acce6f3d
neo1710 <neeraj3dworld@gmail.com>
2023-06-15 23:20:07 +0530

    day2

--- techbazar-app/src/Components/ProductList.tsx
+++ techbazar-app/src/Components/ProductList.tsx
@@ -1,1 +1,17 @@
-import react from "react"
+import react from "react";
+import styled from "@emotion/styled";
+
+export const ProductList=()=>{
+
+   return(
+    <DIV>
+
+    </DIV>
+   ) 
+}
+
+const DIV=styled.div`
+display: grid;
+grid-template-columns: repeat(4,1fr);
+`
+// https://happy-blue-lab-coat.cyclic.app/
--- techbazar-app/src/Redux/store.ts
+++ techbazar-app/src/Redux/store.ts
@@ -1,1 +1,6 @@
 import react from "react"
+import { reducer as productReducer } from "./ProductReducer/reducer"
+
+const rootReducer={
+productReducer
+}
