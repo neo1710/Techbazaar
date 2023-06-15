a1b9f000122f4297aa4feadbb4407fd48cd14ba8
Samay Dhawale <samaydhawale1@gmail.com>
2023-06-15 19:11:09 +0530

    done with navBar and footer

--- techbazar-app/src/Pages/Homepage.tsx
+++ techbazar-app/src/Pages/Homepage.tsx
@@ -1,10 +1,14 @@
+import { Heading } from "@chakra-ui/react"
+import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
 
 export const HomePage=()=>{
 
     return (
-        <div>
+        <div className="HomePage">
            <Navbar /> 
+            <Heading style={{margin:"80px"}}>Hii</Heading>
+           <Footer/>
         </div>
     )
 }
