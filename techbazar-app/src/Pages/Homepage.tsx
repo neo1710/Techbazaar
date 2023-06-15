32b7a91e70257511ced7975c1209ce6c553f725e
Samay Dhawale <samaydhawale1@gmail.com>
2023-06-16 01:16:53 +0530

    done with todays task

--- techbazar-app/src/Pages/Homepage.tsx
+++ techbazar-app/src/Pages/Homepage.tsx
@@ -9,7 +9,7 @@ export const HomePage=()=>{
            <Navbar /> 
             
             <div className="bgPoster">
-                <div style={{width:"500px"}}>
+                <div style={{width:"500px", marginTop:"50px"}}>
                 <Heading fontSize='50px' color='#00472F'>Shopping And Department Store.</Heading>
                 <Text fontSize="18px" marginTop="30px">Shopping is a bit of relaxing hobby from me, which is something troubling for the bank balance</Text>
                 <Button marginTop="30px" borderRadius="50px" background="#00472F" color="#fff" padding="0px 20px 0px 20px" transition=".5s" _hover={{ backgroundColor: "#006040", padding: "2px 22px 2px 22px", transition:".5s" }}>Learn More</Button>
