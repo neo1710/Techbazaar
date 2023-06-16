aad72e99177610a8bd438fd21db2d37720b6f804
Samay Dhawale <samaydhawale1@gmail.com>
2023-06-17 00:39:52 +0530

    done with todays task

--- techbazar-app/src/Pages/Homepage.tsx
+++ techbazar-app/src/Pages/Homepage.tsx
@@ -1,6 +1,15 @@
 import { Button, Heading, Text } from "@chakra-ui/react";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
+import HomePageProductsCard from "../Components/HomePageProductsCard";
+import speaker from "../Images/speaker.svg"
+import bag from "../Images/bag.svg"
+import headphoneBuds from "../Images/headphoneBuds.svg"
+import watch from "../Images/watch.svg"
+import camera from "../Images/camera.svg"
+import pinkHeadphone from "../Images/pinkHeadphone.svg"
+import blackHeadphone from "../Images/blackHeadphone.svg"
+import bottol from "../Images/bottol.svg"
 
 export const HomePage = () => {
   return (
@@ -59,7 +68,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -69,7 +78,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Hand Bag
+              Hand Bag
             </Text>
             <img
               src="https://st3.depositphotos.com/16122460/35400/i/450/depositphotos_354006318-stock-photo-red-stylish-woman-bag-pale.jpg"
@@ -77,7 +86,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -87,7 +96,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Books
+              Books
             </Text>
             <img
               src="https://img.freepik.com/premium-photo/various-fresh-spring-flowers-that-comes-out-book-against-pink-background-love-reading-concept_487960-490.jpg"
@@ -95,7 +104,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -105,7 +114,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Tech
+              Tech
             </Text>
             <img
               src="https://static.vecteezy.com/system/resources/previews/000/310/649/original/yellow-abstract-background-and-a-realistic-computer-laptop-for-business-vector-illustration.jpg"
@@ -113,7 +122,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -123,7 +132,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Sneakers
+              Sneakers
             </Text>
             <img
               src="https://e0.pxfuel.com/wallpapers/762/523/desktop-wallpaper-jordan-1-chicago-red-jordan-shoes-thumbnail.jpg"
@@ -131,7 +140,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -141,7 +150,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Trivel
+              Trivel
             </Text>
             <img
               src="https://img.freepik.com/free-photo/travel-concept-with-baggage-passport_23-2149030573.jpg"
@@ -149,7 +158,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -159,7 +168,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Art
+              Art
             </Text>
             <img
               src="https://chatelaine.com/wp-content/uploads/2019/05/great-running-shoes-810x608-1558535869.jpg"
@@ -167,7 +176,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -177,7 +186,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Education
+              Education
             </Text>
             <img
               src="https://png.pngtree.com/thumb_back/fw800/back_our/20190617/ourmid/pngtree-school-education-and-training-publicity-background-image_127572.jpg"
@@ -185,7 +194,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="absolute"
               zIndex="1"
@@ -195,7 +204,7 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Beauty
+              Beauty
             </Text>
             <img
               src="https://img.freepik.com/premium-photo/makeup-background-beauty-product-color-surface_153977-620.jpg"
@@ -203,7 +212,7 @@ export const HomePage = () => {
             />
           </div>
           <div>
-          <Text
+            <Text
               color="#fff"
               position="fixed"
               zIndex="1"
@@ -213,13 +222,69 @@ export const HomePage = () => {
               marginTop="40px"
               fontWeight="600"
             >
-             Hats
+              Hats
             </Text>
             <img
               src="https://cdn.shopify.com/s/files/1/1859/8979/files/CPI-0161-Inlineimage14.jpg?v=1552012107"
               alt="img"
             />
           </div>
+        </div>
+      </div>
+
+      <div style={{ margin: "60px 80px 0px 80px " }}>
+        <Heading fontSize="24px" margin={"0px 0px 36px 0px"}>
+          Today's Best Deals For You
+        </Heading>
+        <div className="productCards">
+          <HomePageProductsCard
+            image= {speaker}
+            price="$20"
+            text= "Pink Gold Strap, Free Size"
+            title="HomePod Mini"
+          />
+          <HomePageProductsCard
+            image= {watch}
+            price="$20"
+            text= "Pink Gold Strap, Free Size"
+            title="Smart Watch"
+          />
+          <HomePageProductsCard
+            image= {pinkHeadphone}
+            price="$52.30"
+            text= "Pink Gold Strap, Free Size"
+            title="AirPods Max"
+          />
+          <HomePageProductsCard
+            image= {bottol}
+            price="$10"
+            text= "Pink Gold Strap, Free Size"
+            title="Pendletone Water Bottol"
+          />
+          <HomePageProductsCard
+            image= {camera}
+            price="$61"
+            text= "Pink Gold Strap, Free Size"
+            title="Pink Camera"
+          />
+          <HomePageProductsCard
+            image= {bag}
+            price="$11"
+            text= "Pink Gold Strap, Free Size"
+            title="Hand Bags Pink"
+          />
+          <HomePageProductsCard
+            image= {blackHeadphone}
+            price="$9"
+            text= "Pink Gold Strap, Free Size"
+            title="Black & Blue Headphone"
+          />
+          <HomePageProductsCard
+            image= {headphoneBuds}
+            price="$20"
+            text= "Pink Gold Strap, Free Size"
+            title="Black Headphone Buds"
+          />
         </div>
       </div>
       <Footer />
