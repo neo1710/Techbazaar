f36553b5d4f593c8af6e33d6e0ae33e7c15b49ae
ashu7982 <[ashutoshdubey7982@gmail.com]>
2023-06-17 19:45:32 +0530

    done

---
+++ techbazar-app/src/AdminPage.tsx
@@ -0,0 +1,214 @@
+
+
+
+
+// import React, { useState } from 'react';
+
+// const AdminPage = () => {
+//   const [productName, setProductName] = useState('');
+//   const [productDescription, setProductDescription] = useState('');
+//   const [productPrice, setProductPrice] = useState('');
+
+//   const handleFormSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await fetch('https://happy-blue-lab-coat.cyclic.app/products', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           name: productName,
+//           description: productDescription,
+//           price: productPrice,
+//         }),
+//       });
+
+//       if (response.ok) {
+//         // Product added successfully
+//         console.log('Product added successfully');
+//       } else {
+//         // error 
+//         console.error('Error adding product');
+//       }
+//     } catch (error) {
+//       console.error('Error adding product', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Add Product</h2>
+//       <form onSubmit={handleFormSubmit}>
+//         <input
+//           type="text"
+//           placeholder="Product Name"
+//           value={productName}
+//           onChange={(e) => setProductName(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Product Description"
+//           value={productDescription}
+//           onChange={(e) => setProductDescription(e.target.value)}
+//         />
+//         <input
+//           type="number"
+//           placeholder="Product Price"
+//           value={productPrice}
+//           onChange={(e) => setProductPrice(e.target.value)}
+//         />
+//         <button type="submit">Add Product</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AdminPage;
+
+
+
+
+
+// import React, { useState } from 'react';
+
+// const AdminPage: React.FC = () => {
+//   const [productName, setProductName] = useState('');
+//   const [productDescription, setProductDescription] = useState('');
+//   const [productPrice, setProductPrice] = useState('');
+
+//   const handleFormSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await fetch('https://happy-blue-lab-coat.cyclic.app/products', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           name: productName,
+//           description: productDescription,
+//           price: productPrice,
+//         }),
+//       });
+
+//       if (response.ok) {
+//         // Product added successfully, show success message or redirect
+//         console.log('Product added successfully');
+//       } else {
+//         // Handle error cases
+//         console.error('Error adding product');
+//       }
+//     } catch (error) {
+//       console.error('Error adding product', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Add Product</h2>
+//       <form onSubmit={handleFormSubmit}>
+//         <input
+//           type="text"
+//           placeholder="Product Name"
+//           value={productName}
+//           onChange={(e) => setProductName(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           placeholder="Product Description"
+//           value={productDescription}
+//           onChange={(e) => setProductDescription(e.target.value)}
+//         />
+//         <input
+//           type="number"
+//           placeholder="Product Price"
+//           value={productPrice}
+//           onChange={(e) => setProductPrice(e.target.value)}
+//         />
+//         <button type="submit">Add Product</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AdminPage;
+
+
+
+import React, { useState } from 'react';
+import { Box, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+
+const AdminPage: React.FC = () => {
+  const [productName, setProductName] = useState('');
+  const [productDescription, setProductDescription] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://happy-blue-lab-coat.cyclic.app/products', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: productName,
+          description: productDescription,
+          price: productPrice,
+        }),
+      });
+
+      if (response.ok) {
+        // Product added successfully, show success message or redirect
+        console.log('Product added successfully');
+      } else {
+        // Handle error cases
+        console.error('Error adding product');
+      }
+    } catch (error) {
+      console.error('Error adding product', error);
+    }
+  };
+
+  return (
+    <Box>
+      <Heading as="h2" mb={4}>Add Product</Heading>
+      <form onSubmit={handleFormSubmit}>
+        <FormControl>
+          <FormLabel>Product Name</FormLabel>
+          <Input
+            type="text"
+            placeholder="Product Name"
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Product Description</FormLabel>
+          <Input
+            type="text"
+            placeholder="Product Description"
+            value={productDescription}
+            onChange={(e) => setProductDescription(e.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Product Price</FormLabel>
+          <Input
+            type="number"
+            placeholder="Product Price"
+            value={productPrice}
+            onChange={(e) => setProductPrice(e.target.value)}
+          />
+        </FormControl>
+        <Button type="submit" colorScheme="teal" mt={4}>Add Product</Button>
+      </form>
+    </Box>
+  );
+};
+
+export default AdminPage;
--- techbazar-app/src/App.tsx
+++ techbazar-app/src/App.tsx
@@ -1,13 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
    
-    </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import AdminPage from './AdminPage';
+// import SeeProductPage from './SeeProductPage';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/admin">Add Product</Link>
+//           </li>
+//           <li>
+//             <Link to="/products">See Products</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       <Switch>
+//         <Route path="/admin" component={AdminPage} />
+//         <Route path="/products" component={SeeProductPage} />
+//       </Switch>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import AdminPage from './AdminPage';
+// import SeeProductPage from './SeeProductPage';
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/admin">Add Product</Link>
+//           </li>
+//           <li>
+//             <Link to="/products">See Products</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       <Switch>
+//         <Route path="/admin" component={AdminPage} />
+//         <Route path="/products" component={SeeProductPage} />
+//       </Switch>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import AdminPage from './AdminPage';
+import SeeProductPage from './SeeProductPage';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/admin">Add Product</Link>
+          </li>
+          <li>
+            <Link to="/products">See Products</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/products" element={<SeeProductPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
